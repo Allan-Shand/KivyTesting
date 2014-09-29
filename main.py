9522d282b9c3c9d4fcc44d37adf87ba4077418bf
Allan-Shand <ashand79@gmail.com>
2014-09-29 23:24:10 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import kivy
+kivy.require('1.8.0') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.scatter import Scatter
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+
+class HelloWorld(App):
+	def build(self):
+         f = FloatLayout()
+         s = Scatter()
+         l = Label(text="Hello World!",
+              font_size=150)
+              
+         f.add_widget(s)
+         s.add_widget(l)
+         return f
+    
+if __name__ == '__main__':
+    HelloWorld().run()
